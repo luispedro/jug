48ca0f1dbdac0ca8232fe20e01ffb7c3ec57b828
Luís Pedro Coelho <lpc@cmu.edu>
2008-12-22 03:42:56 -0500

    Start nose tests

---
+++ tests/test_lock.py
@@ -0,0 +1,18 @@
+import lock
+def test_twice():
+    assert lock.get('foo')
+    assert not lock.get('foo')
+    lock.release('foo')
+
+    assert lock.get('foo')
+    assert not lock.get('foo')
+    lock.release('foo')
+
+def test_twolocks():
+    assert lock.get('foo')
+    assert lock.get('bar')
+    assert not lock.get('foo')
+    assert not lock.get('bar')
+    lock.release('foo')
+    lock.release('bar')
+
