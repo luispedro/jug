22c37e1efbf345b783309fbad472e06f07a36d94
Luis Pedro Coelho <luispedro@lolitop.(none)>
2008-12-21 21:57:12 -0500

    Add delays for testing

--- jugfile.py
+++ jugfile.py
@@ -1,13 +1,16 @@
 from task import Task, TaskGenerator
+from time import sleep
 
 @TaskGenerator
 def compfeats(url):
     print 'Feats called: ', url
+    sleep(2)
     return url+'feats'
 
 @TaskGenerator
 def nfold(param, feats):
     print 'nfold called: ', param, feats
+    sleep(3)
     return param, feats
 
 imgs = ['images/img1.png','images/img2.png']
