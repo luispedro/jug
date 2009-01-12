a7d9dda6fc6f786e96e3841fd3a63457547a4fb3
Luís Pedro Coelho <lpc@cmu.edu>
2009-01-12 10:55:10 -0500

    Change to MIT License
    Gaël Varoquaux requested this and it opens it up
    for inclusion in Python free scientific software community.

--- jug/jug.py
+++ jug/jug.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2008  Murphy Lab
-# Carnegie Mellon University
-# 
-# Written by Luís Pedro Coelho <lpc@cmu.edu>
+# Copyright (C) 2008, Luís Pedro Coelho <lpc@cmu.edu>
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published
-# by the Free Software Foundation; either version 3 of the License,
-# or (at your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
 #
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
+# The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
 #
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-# 02110-1301, USA.
-#
-# For additional information visit http://murphylab.web.cmu.edu or
-# send email to murphy@cmu.edu
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
 
 from __future__ import division
 
@@ -150,5 +147,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
 # vim: set ts=4 sts=4 sw=4 expandtab smartindent:
