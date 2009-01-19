7b657eecb6924df3631227801e83bfecfbd245b1
Luís Pedro Coelho <lpc@cmu.edu>
2009-01-19 15:56:58 -0500

    Up version number

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ classifiers = [
 ]
 
 setuptools.setup(name = 'Jug',
-      version = '0.2',
+      version = '0.3-git',
       description = 'A Task Based Parallelization Framework',
       long_description = long_description,
       author = 'Luis Pedro Coelho',
