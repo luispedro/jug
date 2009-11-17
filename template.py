cfc03b719d424b72285f34c9d7dad612589c691e
Luís Pedro Coelho <lpc@cmu.edu>
2009-11-17 18:30:35 -0500

    Better template
    The why it's better to have the vimline on top is that it's often useful
    to be able to 'delete until end of file' (which is two key-strokes in
    vim).

--- template.py
+++ template.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2008-2009, Luís Pedro Coelho <lpc@cmu.edu>
+# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 #  of this software and associated documentation files (the "Software"), to deal
@@ -21,4 +22,3 @@
 
 from __future__ import division
 
-# vim: set ts=4 sts=4 sw=4 expandtab smartindent:
