cca847990d95fc6280d021bc85d267bbf57912ce
Luis Pedro Coelho <lpc@cmu.edu>
2010-01-30 18:20:19 -0500

    Update release
    Leapfrog (new release will be soon)

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ copyright = '2008-2010, Luis Pedro Coelho'
 # The short X.Y version.
 version = '0.5'
 # The full version, including alpha/beta/rc tags.
-release = '0.5-rc-1'
+release = '0.5.1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
