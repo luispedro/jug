3995340c08ae3673707d64f1976b82a1af17eaf0
Luis Pedro Coelho <luis@luispedro.org>
2012-08-11 18:49:28 +0100

    TST Increase test coverage

--- jug/tests/test_encode.py
+++ jug/tests/test_encode.py
@@ -1,5 +1,5 @@
 from StringIO import StringIO
-from jug.backends.encode import decompress_stream, encode, decode
+from jug.backends.encode import compress_stream, decompress_stream, encode, decode
 import numpy as np
 def test_encode():
     assert decode(encode(None)) is None
@@ -21,3 +21,24 @@ def test_decompress_stream_seek():
     st.seek(-6, 1)
     second = st.read(8)
     assert first == second[:6]
+
+    st = decompress_stream(StringIO(s))
+    st.seek( 6, 1)
+    st.seek(-6, 1)
+    second = st.read(8)
+    assert first == second[:6]
+
+def test_decompress_stream_readline():
+    text = "1\n2\n3\n4"
+    s = StringIO()
+    cstream = compress_stream(s)
+    cstream.write(text)
+    cstream.flush()
+    dstream = decompress_stream(StringIO(s.getvalue()))
+    assert dstream.readline() == "1\n"
+    assert dstream.readline() == "2\n"
+    assert dstream.readline() == "3\n"
+    assert dstream.readline() == "4"
+    assert dstream.readline() == ""
+    
+     
