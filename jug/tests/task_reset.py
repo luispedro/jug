8c7cf4f40171197bff5c978c23f0ea354670f926
Luis Pedro Coelho <luis@luispedro.org>
2015-06-11 13:13:23 +0200

    TST Reset all hooks in task_reset
    This ensures less contamination in the system

--- jug/tests/task_reset.py
+++ jug/tests/task_reset.py
@@ -1,6 +1,7 @@
 from nose.tools import with_setup
 import jug.task
 from jug.backends.dict_store import dict_store
+from jug.hooks import reset_all_hooks
 
 def _setup():
     jug.task.Task.store = dict_store()
@@ -11,5 +12,6 @@ def _teardown():
     jug.task.Task.store = None
     while jug.task.alltasks:
         jug.task.alltasks.pop()
+    reset_all_hooks()
 
 task_reset = with_setup(_setup, _teardown)
