dde38223894b3265fb897d29e552f256df308c06
Luis Pedro Coelho <luis@luispedro.org>
2016-04-25 16:01:02 +0200

    BLD Make nix recipe buildable

--- default.nix
+++ default.nix
@@ -1,23 +1,26 @@
 #!/usr/bin/env nix-shell
 
 with import <nixpkgs> {};
-with pkgs.python27Packages;
 
-buildPythonPackage { 
-  name = "jugEnv";
-  buildInputs = [
-    git
+let
+  version = "1.2.1";
+in
+
+buildPythonPackage {
+  name = "jug-${version}";
+  buildInputs = with pkgs.python27Packages; [
     python27Full
     python27Packages.virtualenv
     python27Packages.setuptools
     python27Packages.pyyaml
     python27Packages.numpy
     python27Packages.nose
+    python27Packages.redis
     python27Packages.six
-    stdenv
+
     zlib
   ];
-  src = null;
+  src = ./.;
   # When used as `nix-shell --pure`
   shellHook = ''
   unset http_proxy
