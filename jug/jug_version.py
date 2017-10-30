76fb08d562966791ef9782660202e86ce930514f
Luis Pedro Coelho <luis@luispedro.org>
2017-10-30 19:45:13 +0100

    DOC Fix citation information
    Paper is dated 2016, not 2017

--- jug/jug_version.py
+++ jug/jug_version.py
@@ -1,1 +1,6 @@
 __version__ = '1.6.2'
+CITATION = '''\
+    Coelho, L.P., (2017). Jug: Software for Parallel Reproducible Computation in
+    Python. Journal of Open Research Software. 5(1), p.30.
+
+    http://doi.org/10.5334/jors.161O'''
